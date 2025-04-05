bb13b91afb245fd5063ac61211aa82d2e5ca0cf1
LeoDataEngineer <leomarestradabsas@gmail.com>
2025-04-05 11:36:29 -0300

    fiert commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# 📊 ETL Estadísticas de Canales de YouTube
+
+Este proyecto realiza un proceso **ETL** (*Extract, Transform, Load*) para recopilar, transformar y almacenar estadísticas de canales de **YouTube** utilizando la **API oficial de YouTube**.
+Los datos se guardan en archivos **CSV** y también en una base de datos **MySQL** para su posterior visualización mediante **Looker**.
+
+# 🧠 Arquitectura
+
+- **Fuente de datos:** API de YouTube  
+- **Procesamiento:** Python (Pandas)  
+- **Orquestación:** GitHub Actions (Batch ETL)  
+- **Destino:** MySQL Data Warehouse  
+- **Visualización:** Looker Dashboard
+
+![Arq. ETL](imagen/Arq.%20Datos%20ETL-youtube.jpeg)
+
+
+# ⚙️ Tecnologías utilizadas
+
+- **Python**
+- **Pandas**
+- **YouTube Data API v3**
+- **MySQL**
+- **SQLAlchemy**
+- **GitHub Actions** (para automatización del workflow)
+- **Looker** (para dashboards)
+
+# 🚀 Cómo funciona
+
+### 🟡 Extracción (Extract)
+Se obtiene información estadística de una lista de canales de YouTube usando su `channel_id` mediante la API.
+
+### 🔵 Transformación (Transform)
+Se estructuran los datos en un **DataFrame de Pandas**, agregando:
+- Fecha
+- Nombre del canal
+- Vistas
+- Suscriptores
+- Cantidad de videos
+
+### 🟢 Carga (Load)
+- Se guarda un archivo `.csv` en la carpeta `channel_files/` con los datos del día.
+- También se cargan los datos en una base de datos **MySQL**, eliminando previamente los registros con la misma fecha para evitar duplicados.
+
+### 📊 Visualización
+Los datos pueden visualizarse luego en un **dashboard creado en Looker** conectado a la base **MySQL**.
+
+# 🔐 Configuración
+
+Crea un archivo `config.py` con las siguientes variables:
+
+```python
+API_KEY = "TU_API_KEY"
+USER = "usuario_mysql"
+PASSWORD = "tu_contraseña"
+HOST = "localhost"
+PORT = "3306"
+DATABASE = "nombre_base"
+```
+Si usas Github Action guarda las credenciales en la variable de entorno de Github
+
+
+# 📅 Automatización
+
+Puedes automatizar este script con **GitHub Actions** para que se ejecute diariamente y actualice la base de datos de forma automática.
+
+# 📈 Dashboard
+
+Los datos se pueden consumir desde **Looker**, conectando el dashboard directamente a la base de datos **MySQL** donde se almacena la tabla `youtube_stats`.
+
+# ✅ Canales incluidos
+
+El script analiza los siguientes canales de YouTube:
+
+- **Marciaylanube**
+- **TodoCode**
+- **PeladoNerd**
+- **Codigofacilito**
+- **Platzi**
+- **EDteam**
+- **Soy Dalto**
+- **midudev**
+- **freeCodeCamp**
+- **Valen Werle**
+
---
+++ stats_youtube.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import time
+import requests
+import json
+import random
+import os
+from  config import *
+from sqlalchemy import create_engine, text
+
+
+
+api_key = API_KEY
+# Funciones
+# Funcion para obtener los datos de las estadicticas del canal
+# Esta funcion recibe el api_key y el channel_id y devuelve un diccionario con las estadisticas del canal
+def get_stats(api_key,channel_id):
+    
+    url_channel_stats = 'https://youtube.googleapis.com/youtube/v3/channels?part=statistics&id='+channel_id+'&key='+api_key
+    response_channels = requests.get(url_channel_stats)
+    channel_stats = json.loads(response_channels.content)
+    channel_stats = channel_stats['items'][0]['statistics']
+    date = pd.to_datetime('today').strftime("%Y-%m-%d")
+
+    data_channel = {
+
+            'Date':date,
+            'Total_Views':int(float(channel_stats['viewCount'])),
+            'Subscribers':int(float(channel_stats['subscriberCount'])),
+            'Video_count':int(float(channel_stats['videoCount']))
+                        }
+
+    return data_channel
+
+
+# Funcion para obtener los datos de las estadisticas de varios canales 
+def channels_stats(df,api_key):
+    
+    date = []
+    views = []
+    suscriber = []
+    video_count = []
+    channel_name = []
+    
+    tiempo = [1,2.5,2]
+    
+    for i in range(len(df)):
+        
+        stats_temp = get_stats(api_key,df['Channel_id'][i])
+        
+        channel_name.append(df['Channel_name'][i])
+        date.append(stats_temp['Date'])
+        views.append(stats_temp['Total_Views'])
+        suscriber.append(stats_temp['Subscribers'])
+        video_count.append(stats_temp['Video_count'])
+     
+    time.sleep(random.choice(tiempo))
+    
+    data = {
+        
+        'Channel_name':channel_name,
+        'Subscribers':suscriber,
+        'Video_count':video_count,
+        'Total_Views':views,
+        'Createt_at':date,
+    }
+    
+    df_channels = pd.DataFrame(data)
+    
+    return df_channels 
+
+
+# Funcion para guaradr los datos en un archivo csv
+def guardar_csv(df, carpeta='channel_files'):
+    # Crear carpeta si no existe
+    if not os.path.exists(carpeta):
+        os.makedirs(carpeta)
+
+    # Obtener fecha actual en formato YYYY-MM-DD
+    fecha = pd.to_datetime('today').strftime("%Y-%m-%d")
+
+    # Crear nombre de archivo
+    nombre_archivo = f"youtube_stats_{fecha}.csv"
+
+    # Ruta completa
+    ruta_completa = os.path.join(carpeta, nombre_archivo)
+
+    # Guardar DataFrame en CSV
+    df.to_csv(ruta_completa, index=False, encoding='utf-8')
+
+    print(f"Archivo guardado en: {ruta_completa}")
+
+
+
+# guardar los datos en mysql
+# Esta funcion recibe el df y la fecha y guarda los datos en mysql
+def guardar_en_mysql(df, fecha):
+    engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+    tabla = 'youtube_stats'
+
+    with engine.connect() as conn:
+        stmt = text(f"DELETE FROM {tabla} WHERE Createt_at = :fecha")
+        conn.execute(stmt, {"fecha": fecha})
+        conn.commit()  # Esto es importante para que se apliquen los cambios
+
+    df.to_sql(tabla, con=engine, if_exists='append', index=False)
+    print(f"Datos guardados en MySQL para la fecha: {fecha}")
+    
+    
+################### Crear df de canalas para analizar ###################
+
+channels_name  =  ['Marciaylanube','TodoCode','PeladoNerd',
+                   'Codigofacilito', 'Platzi', 'EDteam',
+                   'Soy Dalto', 'midudev', 'freeCodeCamp',
+                   'Valen Werle']
+channels_id  =  ['UCWYUbW_Lw5EQC0WybxInObA','UCz0EXCSvMwYKruljsZjCOzw',
+                 'UCrBzBOMcUVV8ryyAU_c6P5g','UCLXRGxAzeaLDGaOphqapzmg',
+                 'UC55-mxUj5Nj3niXFReG44OQ', 'UCP15FVAA2UL-QOcGhy7-ezA',
+                 'UCtoo4_P6ilCj7jwa4FmA5lQ','UC8LeXCWOalN8SxlrPcG-PaQ',
+                 'UC8butISFwT-Wl7EV0hUK0BQ', 'UCyurPWRyQdR3S0OgPIa8MPA']
+
+channels = {
+    'Channel_name':channels_name,
+    'Channel_id':channels_id}
+
+
+
+if __name__ == "__main__":
+   
+    df_channels = pd.DataFrame(channels)
+
+    print(df_channels)
+
+
+    df = channels_stats(df_channels,api_key)
+    print(df)
+
+    guardar_csv(df, carpeta='channel_files')
+    print("Archivo guardado correctamente")
+
+    guardar_en_mysql(df, df['Createt_at'][0])
+    print("Datos guardados en MySQL correctamente")
+
